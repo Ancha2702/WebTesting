11da01f776168bc387758873885f5019cef5cebf
Ancha2702 <113132853+Ancha2702@users.noreply.github.com>
2023-10-03 21:31:31 +0300

    4дз

---
+++ Work4/send_to_mail.py
@@ -0,0 +1,33 @@
+import smtplib
+from os.path import basename
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from time import sleep
+
+sleep(1)
+fromaddr = "testgb113@mail.ru"
+toaddr = "testgb113@mail.ru"
+mypass = "LTkQAMzizYE4zHcYgpzh"
+reportname = "log.txt"
+
+
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = "Сообщения по дз4"
+text = "Посмотри"
+
+msg.attach(MIMEText(text))
+
+with open(reportname, "rb") as f:
+    part = MIMEApplication(f.read(), Name=basename(reportname))
+    part['Content-Disposition'] = f'attachment; filename="{basename(reportname)}"'
+    msg.attach(part)
+
+
+server = smtplib.SMTP_SSL('smtp.mail.ru', 465)
+server.login(fromaddr, mypass)
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
+server.quit()
